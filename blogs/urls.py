17f7c4345b22d0b3f59b67b1bb1ef9eb25443888
g-chandran <chandranchan321@gmail.com>
2020-08-30 18:50:11 +0530

    User Account management

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, SignupView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('signup/', SignupView.as_view(), name='signup'),  
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+  template_name = "home.html"
+
+class SignupView(generic.CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy('home')
+  template_name = 'signup.html'
