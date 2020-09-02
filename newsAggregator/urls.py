4cfb65cab915b94a3bfbe0454e2ad46051f5b0bf
g-chandran <chandranchan321@gmail.com>
2020-09-02 19:31:03 +0530

    heroku config

--- newsAggregator/urls.py
+++ newsAggregator/urls.py
@@ -22,8 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blogs.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
